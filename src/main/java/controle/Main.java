9c3d2f18a28c501e7beb53398f174e1af97a44f1
Dodivargas <dodi_inter@hotmail.com>
2017-10-24 22:15:04 -0200

    Conectando com o mySql

--- src/main/java/controle/Main.java
+++ src/main/java/controle/Main.java
@@ -2,29 +2,47 @@ package controle;
 
 import modelo.Atividade;
 import modelo.Pessoa;
+import database.ConectaMysql;
+
+import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        Pessoa pessoa = new Pessoa("pele");
-        Atividade atividade = new Atividade("futebol","14:00","15:00","Lazer");
-        Atividade atividade2 = new Atividade("mortalpratras","11:00","12:00","Lazer");
-        Atividade atividade3 = new Atividade("videogame","16:00","17:00","Lazer");
-        Atividade atividade4 = new Atividade("dale","13:00","14:00","Lazer");
+//        Pessoa pessoa = new Pessoa("pele");
+//        Atividade atividade = new Atividade("futebol","14:00","15:00","Lazer");
+//        Atividade atividade2 = new Atividade("mortalpratras","11:00","12:00","Lazer");
+//        Atividade atividade3 = new Atividade("videogame","16:00","17:00","Lazer");
+//        Atividade atividade4 = new Atividade("dale","13:00","14:00","Lazer");
+//
+//        pessoa.adicionaAtividadeDia(atividade);
+//        pessoa.adicionaAtividadeDia(atividade2);
+//        pessoa.adicionaAtividadeDia(atividade3);
+//        pessoa.adicionaAtividadeDia(atividade4);
+//        pessoa.removeAtividadeDia("dale");
+//
+//
+//        Atividade a = new Atividade("natação","15:00","16:00","Lazer");
+//        pessoa.editaAtividade("futebol",a);
+//
+//
+//        System.out.println(pessoa.getAtividades().toString());
 
-        pessoa.adicionaAtividadeDia(atividade);
-        pessoa.adicionaAtividadeDia(atividade2);
-        pessoa.adicionaAtividadeDia(atividade3);
-        pessoa.adicionaAtividadeDia(atividade4);
-        pessoa.removeAtividadeDia("dale");
+
+            Connection con = ConectaMysql.getConnection();
+
+            ConectaMysql.fechaConexao(con);
 
 
-        Atividade a = new Atividade("natação","15:00","16:00","Lazer");
-        pessoa.editaAtividade("futebol",a);
+            System.out.println(ConectaMysql.statusConection());
 
 
-        System.out.println(pessoa.getAtividades().toString());
+
+
+
+
+
 
 
     }
---
+++ src/main/java/database/ConectaMysql.java
@@ -0,0 +1,99 @@
+package database;
+//Classes necessárias para uso de Banco de dados //
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+
+import java.sql.PreparedStatement;
+
+import java.sql.ResultSet;
+
+//Início da classe de conexão//
+
+public class ConectaMysql{
+
+    private static final  String driver = "com.mysql.jdbc.Driver";
+    private static final  String url = "jdbc:mysql://localhost:3306/agenda";
+    private static final  String user = "root";
+    private static final  String senha = "dodivargas";
+    private static String status = "";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+
+            Class.forName(driver);
+
+            connection = DriverManager.getConnection(url,user,senha);
+
+            if (connection != null) {
+
+                status = ("STATUS--->Conectado com sucesso!");
+
+            } else {
+
+                status = ("STATUS--->Não foi possivel realizar conexão");
+
+            }
+
+            return connection;
+
+        } catch (ClassNotFoundException | SQLException e ){
+
+            throw new RuntimeException("Erro na conexão: ",e);
+
+        }
+
+
+    }
+
+    public static void fechaConexao(Connection con){
+
+        try {
+            if(con!=null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        status = ("Conectou e foi fechado");
+
+    }
+
+    public static void fechaConexao(Connection con, PreparedStatement stmt){
+
+        fechaConexao(con);
+        try {
+            if(stmt!=null){
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        status = ("Conectou e foi fechado");
+    }
+
+    public static void fechaConexao(Connection con, PreparedStatement stmt, ResultSet rs){
+
+        fechaConexao(con,stmt);
+
+        try {
+            if(rs != null){
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        status = ("Conectou e foi fechado");
+    }
+
+    public static String statusConection() {
+
+        return status;
+
+    }
+
+}
